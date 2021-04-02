459e08ab7a14ef213d997daeb250f04f71eadbea
Ivo <playoso123@gmail.com>
2021-04-01 23:29:50 -0300

    Sistemas Embebidos

---
+++ Tareas/Readme.md
@@ -0,0 +1,2 @@
+# Curso IEE507
+# Grupo 14
---
+++ Tareas/tarea3/ejercicio2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#define byte 8
+using namespace std;
+
+float temperatura(float n){
+	float t;
+	t = (n/4)+40;
+	return t;
+}
+void conversion(float n){
+	float t = temperatura(n);
+	cout << "La temperatura en grados Celsius es:  " << ((t-32) * 5/9)<< endl;
+		
+}
+int main(){
+	int n;
+	
+	cout << "Cuantas veces hicieron sonar la chicharra en un minuto?:  "; cin>>n;
+	float t = temperatura(n);
+	
+	cout << "La temperatura en grados Fahrenheit es:  " << t << endl;
+	conversion(n);
+
+	return 0;
+
+}
